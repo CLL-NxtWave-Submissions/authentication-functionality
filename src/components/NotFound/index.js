fe4a5851da4ce14353e547f1dcd3c695c02e944b
Surya T N <suryatej.nim@yahoo.com>
2022-10-14 08:22:06 +0000

    Implemented JSX elements in NotFound component, with CSS styling to render the intended UI layout

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,9 @@
-// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-bg-container">
+    <h1 className="not-found-header">Not Found</h1>
+  </div>
+)
+
+export default NotFound
