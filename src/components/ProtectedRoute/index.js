4024d53f710177c77f67b6ed02d1ee14598ac410
Surya T N <suryatej.nim@yahoo.com>
2022-10-14 07:04:01 +0000

    Implemented ProtectedRoute wrapper component with logic to check user authentication and accordingly navigate to login route or respective requested route

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,1 +1,14 @@
-// Write your JS code here
+import {Redirect, Route} from 'react-router-dom'
+import Cookie from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const jwtTokenValue = Cookie.get('jwt_token')
+
+  if (jwtTokenValue === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
